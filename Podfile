7fd16896e9121d7774f226b6572e797d1d440bb0
khnshr <shehryark@gmail.com>
2018-08-30 15:22:23 +0200

    Added cocoapods

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+source 'https://github.com/CocoaPods/Specs.git'
+target 'mypepp' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'GoogleMaps'
+  pod 'GooglePlaces'
+  pod 'Firebase/Core'
+
+  # Pods for mypepp
+
+  target 'mypeppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'mypeppUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- mypepp/ViewController.swift
+++ mypepp/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Pepp: UILabel!
+    @IBOutlet weak var Email: UITextField!
+    @IBOutlet weak var Pass: UITextField!
+    @IBOutlet weak var LoginBtn: UIButton!
+    //var MapView: GMSMapView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func loadView() {
+        let camera = GMSCameraPosition.camera(withLatitude: 55.32, longitude: 18.06, zoom: 10.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        // marker
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D( latitude: 55.32, longitude: 18.06)
+        marker.title = "Stockholm"
+        marker.snippet = "Sweden"
+        marker.map = mapView
+        
+        //navigationItem.rightBarButtonItem = UIBarButtonItem( title: "Next", style: .Plain, target: self, action: "next")
+    }
+
+    //func next (){
+    //    let nextlocation = CLLocationCoordinate2D( latitude: 55.34, longitude: 18.04)
+    //    mapView?.camera = GMSCameraPosition.camera(nextlocation.latitude, nextlocation.longitude, zoom: 12.0)
+        
+    //    let marker = GMSMarker(position: nextlocation)
+    //    marker.title = "Kickboxing"
+    //    marker.map = mapView
+    //}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func Email(_ sender: UITextField) {
+        Email.text = sender.text
+    }
+    
+    @IBAction func Pass(_ sender: UITextField) {
+        Pass.text = sender.text
+    }
+    
+    
 }
 
